a40ce8e8374b42606d857d2f6ff8b842f77ad49b
cxzzxcq1 <eqwe5943@gmail.com>
2025-06-28 22:42:12 +0200

    first commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,12 +12,39 @@ const Home = ({ userData }) => {
   return (
     <div className="flexcontHome">
       <div className="divgreit">
-        <h1 className="h1greit">Добро пожаловать, {userData.username}</h1>
-        <h2>Выберите роль:</h2>
+        <div className="conteinerlogo">
+          <img
+            className="logoimg"
+            src="https://gglead.net/skin/ggleadv2/ru/images/tild3736-3036-4766-b163-333533623537__favicon_4.ico"
+          ></img>
+          <h1 className="conteinerlogotitl">GGselGift</h1>
+        </div>
+
+        {/* <h2>Выберите роль:</h2>
         <button onClick={() => navigate("/seller/step1")}>Я продавец</button>
         <button onClick={() => navigate("/buyer/enter-code")}>
           Я покупатель
-        </button>
+        </button> */}
+      </div>
+      <div className="contbakdrtit">
+        <h1 className="h1greit">
+          Добро пожаловать, {userData.username} в{" "}
+          <span className="span_h1greit">GGselGift</span> — ваш надежный
+          помощник в продаже NFT-подарков!
+        </h1>
+      </div>
+
+      <div className="contbakdrtitptit">
+        <p className="ptit">
+          <span className="span_h1greit">GGselGift</span> — это безопасная и
+          удобная платформа, созданная специально для продажи NFT-подарков.
+          <br />
+          <div className="slidecontbakdrtitptit"></div>
+          Мы выступаем гарантом сделки между продавцом и покупателем,
+          <br />
+          обеспечивая обе стороны защитой, прозрачностью и простотой
+          взаимодействия.
+        </p>
       </div>
     </div>
   );
