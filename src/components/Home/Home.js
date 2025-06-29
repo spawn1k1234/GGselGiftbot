3eb5e02f55c4f3fb985ff6e067553187af88631b
cxzzxcq1 <eqwe5943@gmail.com>
2025-06-29 14:19:21 +0200

    first commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,7 +17,10 @@ const Home = ({ userData }) => {
             className="logoimg"
             src="https://gglead.net/skin/ggleadv2/ru/images/tild3736-3036-4766-b163-333533623537__favicon_4.ico"
           ></img>
-          <h1 className="conteinerlogotitl">GGselGift</h1>
+          <h1 className="conteinerlogotitl">
+            GGselGift{" "}
+            <span className="spanggiftuser">- {userData.username}</span>
+          </h1>
         </div>
 
         {/* <h2>Выберите роль:</h2>
@@ -28,12 +31,98 @@ const Home = ({ userData }) => {
       </div>
       <div className="contbakdrtit">
         <h1 className="h1greit">
-          Добро пожаловать, {userData.username} в{" "}
-          <span className="span_h1greit">GGselGift</span> — ваш надежный
-          помощник в продаже NFT-подарков!
+          Добро пожаловать, {userData.username}
+          <br></br>в <span className="span_h1greit">GGselGift</span> — ваш
+          надежный помощник в продаже NFT-подарков!
         </h1>
       </div>
+      <div class="slider">
+        <div class="slide-track">
+          <a href="#">
+            <img
+              src="https://images.icon-icons.com/3261/PNG/512/steam_logo_icon_206670.png"
+              alt="Image 1"
+            />
+            Steam
+          </a>
+          <a href="#">
+            <img
+              src="https://cdn-icons-png.freepik.com/512/7/7719.png"
+              alt="Image 2"
+            />
+            GGselGift
+          </a>
+          <a href="#">
+            <img
+              src="https://zefirka.club/uploads/posts/2023-01/1673605353_6-zefirka-club-p-ikonka-telegramma-chernaya-7.png"
+              alt="Image 3"
+            />
+            Telegram
+          </a>
+          <a href="#">
+            <img
+              src="https://icons.veryicon.com/png/o/commerce-shopping/poly-budget-icon-library/gift-111.png"
+              alt="Image 4"
+            />
+            GiftTON
+          </a>
+          <a href="#">
+            <img
+              src="https://images.icon-icons.com/2428/PNG/512/epic_games_black_logo_icon_147139.png"
+              alt="Image 5"
+            />
+            EpicGames
+          </a>
 
+          <a href="#">
+            <img
+              src="https://images.icon-icons.com/3261/PNG/512/steam_logo_icon_206670.png"
+              alt="Image 1"
+            />
+            Steam
+          </a>
+          <a href="#">
+            <img
+              src="https://cdn-icons-png.freepik.com/512/7/7719.png"
+              alt="Image 2"
+            />
+            GGselGift
+          </a>
+          <a href="#">
+            <img
+              src="https://zefirka.club/uploads/posts/2023-01/1673605353_6-zefirka-club-p-ikonka-telegramma-chernaya-7.png"
+              alt="Image 3"
+            />
+            Telegram
+          </a>
+          <a href="#">
+            <img
+              src="https://icons.veryicon.com/png/o/commerce-shopping/poly-budget-icon-library/gift-111.png"
+              alt="Image 4"
+            />
+            GiftTON
+          </a>
+          <a href="#">
+            <img
+              src="https://images.icon-icons.com/2428/PNG/512/epic_games_black_logo_icon_147139.png"
+              alt="Image 5"
+            />
+            EpicGames
+          </a>
+        </div>
+      </div>
+      <div className="flexscontthear">
+        <div className="etxdiv"></div>
+        <div className="etxdiv"></div>
+        <div className="etxdiv"></div>
+        <div className="etxdiv"></div>
+        <div className="etxdiv"></div>
+        {/* <div className="etxdiv"></div> */}
+      </div>
+      {/* <img
+        className="imgmain"
+        src="https://c-p.rmcdn1.net/5bf7ef2ff5d18d046b6e678d/2810850/upload-ddd270af-e36a-425b-88d1-485a0b515133.gif"
+      ></img> */}
       <div className="contbakdrtitptit">
         <p className="ptit">
           <span className="span_h1greit">GGselGift</span> — это безопасная и
